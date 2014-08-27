33f0f8b9b698dbfe44ccb83ef06cb012ef4c6ef2
Joseph Fox-Rabinovitz <jfoxrabinovitz@gmail.com>
2014-08-27 02:15:30 -0500

    Copyright year: being anal

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 ##
 ## The MIT License (MIT)
 ##
-## Copyright (c) 2013 by Joseph Fox-Rabinovitz
+## Copyright (c) 2014 by Joseph Fox-Rabinovitz
 ##
 ## Permission is hereby granted, free of charge, to any person obtaining a copy
 ## of this software and associated documentation files (the "Software"), to deal
